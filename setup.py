bfdb614b7b3e77ff2f854d91cce8239b94b6f578
gewbot <53042502+gewbot@users.noreply.github.com>
2020-01-10 14:08:23 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -124,6 +124,13 @@ os.system('sudo chmod 777 //home/pi/startup.sh')
 
 replace_num('/etc/rc.local','fi','fi\n//home/pi/startup.sh start')
 
+try: #fix conflict with onboard Raspberry Pi audio
+	os.system('sudo touch /etc/modprobe.d/snd-blacklist.conf')
+	with open("/etc/modprobe.d/snd-blacklist.conf",'w') as file_to_write:
+		file_to_write.write("blacklist snd_bcm2835")
+except:
+	pass
+
 print('树莓派中的程序已经安装完毕，已经断开连接并重启。\n你现在可以将树莓派断电来安装摄像头以及驱动板(Robot HAT)。\n再次开机后树莓派会自动运行程序将舵机口信号设置为使舵机转动到中间位置，方便机械组装。')
 
 os.system("sudo reboot")
